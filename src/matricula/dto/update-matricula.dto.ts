31064ef1ac4259b158792a0af870f1d7fa35544b
DouglaSiewers <douglassiewers@gmail.com>
2022-07-16 09:25:12 -0300

    Corrigindo Get de matricula pelo Redis

--- src/matricula/dto/update-matricula.dto.ts
+++ src/matricula/dto/update-matricula.dto.ts
@@ -4,4 +4,10 @@ import { IsString } from 'class-validator';
 export class UpdateMatriculaDTO extends Matricula {
   @IsString()
   turma: string;
+
+  @IsString()
+  curso: string;
+
+  @IsString()
+  dt_ingresso: string;
 }
